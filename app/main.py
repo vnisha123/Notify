f3afa1ae8647865eebb3e608d5bdbf74ad7c370f
Nisha <nishavahutre001@gmail.com>
2026-04-24 20:27:52 +0530

    create an endroute to test pydantic model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.core.models import NotificationRequest
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return{"message": "welcome to notify"}
+
+@app.post("/notify")
+async def send_notification(payload : NotificationRequest):
+    return{"data":payload}
