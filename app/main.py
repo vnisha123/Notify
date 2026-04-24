0971e859b75b826fd616a2d07b7c1c5025166680
Nisha <nishavahutre001@gmail.com>
2026-04-24 21:48:46 +0530

    connected to the database

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.core.models import NotificationRequest
+from app.db_config.db import engine, Base
+from app.db_config.models import NotificationTable
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
